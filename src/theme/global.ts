b6bdf2c34de85c941d7de47bc2eebe1c2f7224a6
DevairS <devairaugustodasilva@gmail.com>
2022-01-08 11:54:16 -0300

    Global style

---
+++ src/theme/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Roboto', sans-serif;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+  }
+  input, button {
+    font-family: 'Roboto', sans-serif;
+  }
+  button {
+    cursor: pointer;
+  }
+
+  div {
+    display: flex;
+  }
+`;
